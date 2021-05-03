15b5c08b9df4b4a4143805d40caf6a12a3658ea0
ivobul <24499940+ivobul@users.noreply.github.com>
2021-05-03 15:18:04 -0400

    Add files via upload

--- madison-square-market/main.js
+++ madison-square-market/main.js
@@ -1,26 +1,31 @@
 $(document).ready(() => {
   // cart navigation item
-  $('#cart').on('click', () => {
+  $('#cart').on('click', (e) => {
     $('#cartMenu').show();
+    $(e.currentTarget).find('.arrow-down').addClass('rotate');
   });
-  $('.dropdown-menu').on('mouseleave', () => {
-    $('.dropdown-menu').hide();
+  $('#cart .dropdown-menu').on('mouseleave', (e) => {
+   $('#cartMenu').hide();
+   $(e.currentTarget).prev().find('.arrow-down').removeClass('rotate');
   });
   
   // account navigation item
-  $('#account').on('click', () => {
+  $('#account').on('click', (e) => {
     $('#accountMenu').show();
+    $(e.currentTarget).find('.arrow-down').addClass('rotate');
   });
-  $('.dropdown-menu').on('mouseleave', () => {
-    $('.dropdown-menu').hide();
+  $('#account .dropdown-menu').on('mouseleave', (e) => {
+    $('#account .dropdown-menu').hide();
+    $(e.currentTarget).prev().find('.arrow-down').removeClass('rotate');
   });
 
   // help navigation item
-  $('#help').on('click', () => {
+  $('#help').on('click', (e) => {
     $('#helpMenu').show();
+    $(e.currentTarget).find('.arrow-down').addClass('rotate');
   });
-  $('.dropdown-menu').on('mouseleave', () => {
-    $('.dropdown-menu').hide();
+  $('#help .dropdown-menu').on('mouseleave', (e) => {
+    $('#help .dropdown-menu').hide();
+    $(e.currentTarget).prev().find('.arrow-down').removeClass('rotate');
   });
 });
-  
